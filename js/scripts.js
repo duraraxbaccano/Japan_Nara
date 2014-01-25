38df225af278b3eb1b7c4e998e50fa1d9f9324fb
Leon.Lin <ientin.lin@gmail.com>
2014-01-26 03:10:28 +0800

    initial Japan Trip Nara

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+
+  $('p#menubar a[data-target]').click(function() {
+      var moveTo=$($(this).attr("data-target"));
+
+        $('html,body').animate({
+          scrollTop: moveTo.offset().top
+        }, 1000);
+  });
+
+};
